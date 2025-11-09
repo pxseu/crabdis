aa0e5d8f29bcb3be131d4c5321f2def6ccdeab83
pxseu <57842793+pxseu@users.noreply.github.com>
2025-11-09 06:04:50 +0100

    bump criterion

--- benches/int_parser.rs
+++ benches/int_parser.rs
@@ -1,4 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
+use std::hint::black_box;
 use std::io::Cursor;
 use tokio::runtime::Runtime;
 
